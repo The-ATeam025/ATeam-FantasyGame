e30a5a610d05dde999e2932dc73945de562712a7
jack giovenco <jackgio2002@hotmail.com>
2023-11-02 14:26:27 -0400

    Merged from the Banquet Hall Branch

--- ATeam-FantasyGame/better-dialogue.cpp
+++ ATeam-FantasyGame/better-dialogue.cpp
@@ -25,7 +25,7 @@ DialogueTree::DialogueTree() //constructor
 
 void DialogueTree::init() //set up tree with dialogue, make this one virtual?
 {
-	cout << "As you eneter the room, you see a group of figures around a table laden with all sorts of delicious foods and wines. This is odd, because the rest of the room is dusty and abandoned, but what is even stranger than the food are the people seated there. " << endl;
+	cout << "As you enter the room, you see a group of figures around a table laden with all sorts of delicious foods and wines. This is odd, because the rest of the room is dusty and abandoned, but what is even stranger than the food are the people seated there. " << endl;
 
 	cout << "A group of humanoid creatures with shimmering wings are enjoying the feast, but there is a sinister air that contrasts with their beautiful appearance.You get the feeling that you do not want to mess with them. As you stand hesitantly in the corner, one fairy notices you." << endl;
 
