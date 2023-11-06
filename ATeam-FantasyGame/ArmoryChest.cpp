474a0292df05ddbc9527e8fbc92dc12281b49855
jack giovenco <jackgio2002@hotmail.com>
2023-11-06 18:10:02 -0500

    ATEAM-72 fixed issue throwing a nullpointer if the player does not have anything equipped to their hands

--- ATeam-FantasyGame/ArmoryChest.cpp
+++ ATeam-FantasyGame/ArmoryChest.cpp
@@ -6,16 +6,27 @@ ArmoryChest::ArmoryChest(string newName, string newDescription)
 }
 
 void ArmoryChest::interactWithObject(Player& player) {
+    if (!isLocked) {
+        // The chest is already open
+        if (items.empty()) {
+            cout << "The chest is empty." << endl;
+        }
+        return;
+    }
+
     Item* rustyKey = player.getWeaponSlot();
 
-    if (isLocked) {
+    if (rustyKey == nullptr) {
+        cout << "The chest won't budge. You will need to find something to unlock it." << endl;
+    }
+    else {
         if (rustyKey->getName() == "Rusty Key") {
             // The player has the rusty key, unlock the chest
             isLocked = false;
             setDescription("An unlocked chest in the armory.");
-            cout << "You use the Key to unlock the chest." << endl << endl;
+            cout << "You use the key to unlock the chest." << endl << endl;
             // The chest is unlocked, allow the player to retrieve items
-            cout << "You open the chest and find a sword and a helmet inside. You place them in your inventory." << endl;
+            cout << "You open the chest and find a sword and a helmet inside. You take them with you." << endl;
 
             // Add items to the player's inventory
             giveItemsToPlayer(player);
@@ -25,10 +36,4 @@ void ArmoryChest::interactWithObject(Player& player) {
             cout << "The chest won't budge. You will need to find something to unlock it." << endl;
         }
     }
-    else {
-        if (items.empty()) {
-            cout << "The chest is empty." << endl;
-        }
-    }
 }
-
