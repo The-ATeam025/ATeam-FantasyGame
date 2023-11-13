37635b79f5c7c50398a074a51b78601cd0977a8c
Weronika-Przedworska <144631559+Weronika-Przedworska@users.noreply.github.com>
2023-11-13 17:50:22 -0500

    Hare will only talk if you are holding a carrot

--- ATeam-FantasyGame/hareDialogue.cpp
+++ ATeam-FantasyGame/hareDialogue.cpp
@@ -7,15 +7,31 @@ hareDialogue::hareDialogue() : NPCDefeated(false), talkedTo(false) {
 }
 
 void hareDialogue::startDialogue(Player& player) {
+	Item* hands = player.getWeaponSlot();
+
+
 	if (talkedTo == false) {
 		talkedTo = true;
-		dtree.init_hare();
+
+		if (hands) {
+			string handName = hands->getName();
+			if (handName == "carrot") {
+				dtree.init_hare();
+			}
+		}
+		else
+		{
+			cout << "Hunfry" << endl;
+		}
 	}
-	else if (talkedTo == true) {
-		cout << "The hare looks in your direction, but it has nothing else to tell you." << endl;
+		else if (talkedTo == true) {
+			cout << "The hare twitches its nose at you." << endl;
+		}
 	}
-}
+
 
 void hareDialogue::defeatedDialogue() {
 	cout << "You defeeated the hare." << endl;
 }
+
+
