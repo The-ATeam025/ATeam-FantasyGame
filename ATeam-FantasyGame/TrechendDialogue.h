2edd37d86c06fd36f2d738e94687bc526bda56ad
jack giovenco <jackgio2002@hotmail.com>
2023-11-20 13:23:48 -0500

    ATEAM-76 Set up class for fighting the creature. Allows for the user to attack the monster based on timed responses. If the user waits too long, they will get hit by a random attack. If the user gets hit 3 times, they lose the fight. If they hit the monster 5 times, they win the battle.

---
+++ ATeam-FantasyGame/TrechendDialogue.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "DialogueNPC.h"
+#include "Item.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+#include <thread>
+#include <algorithm>
+#include <cctype>
+#include <vector>
+
+class TrechendDialogue : public DialogueNPC {
+private:
+    int maxHealth;
+    int maxAttempts;
+    int health;
+    bool defeated;
+
+    
+
+public:
+    TrechendDialogue();
+    ~TrechendDialogue();
+
+    void resetBattle();
+    void promptUserForInstructions();
+    void promptUserWithWords();
+    void processUserInput(Player& player);
+    void dealDamage();
+    bool checkVictory();
+    void defeatTrechend();
+    void hitPlayerRandomly(Player& player);
+    void handleLoss(Player& player);
+    void startDialogue(Player& player) override;
+    void defeatedDialogue() override;
+};
