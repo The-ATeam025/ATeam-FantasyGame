dc03a511eb74e4a115550ffda73f2ec9232da249
jack giovenco <jackgio2002@hotmail.com>
2023-11-20 15:28:28 -0500

    ATEAM-76 added commenting, fixed an issue with replaying the encounter after you took damage in the first fight

--- ATeam-FantasyGame/TrechendDialogue.h
+++ ATeam-FantasyGame/TrechendDialogue.h
@@ -10,6 +10,37 @@
 #include <cctype>
 #include <vector>
 
+/*
+    TrechendDialogue();  - Constructor to initialize member variables
+
+    ~TrechendDialogue(); - Destructor (if needed)
+
+
+
+    void resetBattle();  - Reset the battle state
+
+    void promptUserForInstructions(Player& player); - Display initial instructions to the player
+
+    std::string promptUserWithWords(); - Generate and display a word for the player to type
+
+    void processUserInput(Player& player); - Process the player's input during the battle
+
+    void dealDamage();   - Decrease the monster's health and display corresponding messages
+
+    bool checkVictory(); - Check if the player has defeated the monster
+
+    void defeatTrechend();      - Display victory message and mark the monster as defeated
+
+    void hitPlayerRandomly(Player& player); - Simulate a random attack from the monster
+
+    void handleLoss(Player& player);   - Handle the player's loss in the battle
+
+    void startDialogue(Player& player) override;   - Start the dialogue and initiate the battle
+
+    void defeatedDialogue() override;  - Display dialogue when the monster is defeated
+
+*/
+
 class TrechendDialogue : public DialogueNPC {
 private:
     int maxHealth;
