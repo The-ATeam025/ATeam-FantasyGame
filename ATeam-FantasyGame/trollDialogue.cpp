f7869d8831b004c9dfa8cb1af2f91d9f5ecd96aa
𝖒𝖊𝖑𝖎 🦋 <galaxyclouds10@gmail.com>
2023-12-02 20:36:20 -0500

    added input validation for the troll bridge

--- ATeam-FantasyGame/trollDialogue.cpp
+++ ATeam-FantasyGame/trollDialogue.cpp
@@ -1,5 +1,10 @@
 #include "trollDialogue.h"
 #include <iostream>
+//both files below are used for riddle answers, making sure there is no case sensitivity
+#include <algorithm> // for std::transform
+#include <cctype>    // for ::tolower
+#include <limits> // The <limits> header is included to allow us to use std::numeric_limits. This is necessary for the std::cin.ignore line, to ensure we clear out the entire input buffer.
+
 
 using namespace std;
 
@@ -9,22 +14,31 @@ trollDialogue::trollDialogue() : NPCDefeated(false), talkedTo(false) {
 void trollDialogue::startDialogue(Player& player) {
 	string magicWord = " ";
 
+	// Before reading the answer using std::getline, we clear the input buffer to ensure no leftover characters (like newline from a previous input) are read as the answer.
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
 	if (talkedTo == false) {
 		cout << "As you cautiously step onto the creaking wooden planks of the bridge, a deep, growling voice erupts, stopping you in your tracks. Then suddenly, the short hairy creature walks towards you, almost scaring you to death. " << endl;
 		
 	}
-
 	cout << "Troll: WHAT'S THE MAGIC WORD?!?!" << endl;
-	cin >> magicWord;
-	if (magicWord == "rathad") {
-		talkedTo = true;
-		dtree.init_troll(player);
-	}
-	else {
+
+	std::getline(std::cin, magicWord);	//reads the whole line of input, including spaces, until it encounters a newline character.
+
+	//code to accept the user answer and transform answer into all lowercase, no matter how it is written.
+	std::string answerLowerCase = magicWord; // Make a copy of answer
+	std::transform(answerLowerCase.begin(), answerLowerCase.end(), answerLowerCase.begin(),
+		[](unsigned char c) { return std::tolower(c); });
+
+	if (answerLowerCase != "rathad") {
 		cout << "The troll stand there with his arms crossed. Waiting for an answer." << endl;
 		if (talkedTo == true) {
-			cout << "The troll looks in your direction, but it have nothing else to tell you" << endl;
+			cout << "The troll looks in your direction, but it has nothing else to tell you" << endl;
 		}
+	}
+	else {
+		talkedTo = true;
+		dtree.init_troll(player);
 	}
 }
 
