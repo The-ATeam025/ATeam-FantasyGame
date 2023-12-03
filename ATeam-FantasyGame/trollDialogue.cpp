8a22cc37baf5c53bbd2443856e01017cd2599948
Weronika-Przedworska <144631559+Weronika-Przedworska@users.noreply.github.com>
2023-12-02 20:46:36 -0500

    Fixed logic so troll doesnt ask for magic word if it is already given (e.g in the case the player doesn't have the vine and has to go back to the meadow).

--- ATeam-FantasyGame/trollDialogue.cpp
+++ ATeam-FantasyGame/trollDialogue.cpp
@@ -6,8 +6,10 @@ using namespace std;
 trollDialogue::trollDialogue() : NPCDefeated(false), talkedTo(false) {
 }
 
+
 void trollDialogue::startDialogue(Player& player) {
 	string magicWord;
+
 
 	if (talkedTo == false) {
 		talkedTo = true;
@@ -16,26 +18,33 @@ void trollDialogue::startDialogue(Player& player) {
 		cin >> magicWord;
 		if (magicWord == "rathad") {
 			talkedTo = true;
+			convoCompleted = true;
 			dtree.init_troll(player);
 		}
 		else {
 			cout << "Troll: Wrong! Go away and come back when you learn something!!" << endl;
 		}
 	}
-	else
+	else if((talkedTo==true) && (convoCompleted==false))
 	{
-		cout << "The troll is still there, leaning against the bridge and tapping its foot impatiently." << endl;
+		cout << "The troll is leaning against the bridge and tapping its foot impatiently." << endl;
 		cout << "Troll:" << randomResponse() << endl;
 		cin >> magicWord;
 		if (magicWord == "rathad") {
 			talkedTo = true;
+			convoCompleted = true;
 			dtree.init_troll(player);
 		}
 		else {
 			cout << "Troll: Wrong again!" << endl;
 			
 		}
-	};
+	}
+	//
+	else if (convoCompleted==true)
+	{
+		cout << "Vine test!" << endl;
+	}
 	
 }
 
@@ -73,3 +82,14 @@ string trollDialogue::randomResponse()
 	return reply;
 }
 
+bool trollDialogue::checkConvoCompleted()
+{
+	if (convoCompleted == false)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
