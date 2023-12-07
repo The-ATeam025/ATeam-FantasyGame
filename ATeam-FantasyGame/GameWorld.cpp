2476739cb781272974e7b2fffed5cf313b20dde3
𝖒𝖊𝖑𝖎 🦋 <galaxyclouds10@gmail.com>
2023-12-07 17:02:06 -0500

    changed crossed lake location description

--- ATeam-FantasyGame/GameWorld.cpp
+++ ATeam-FantasyGame/GameWorld.cpp
@@ -28,8 +28,8 @@ void GameWorld::init(Player& player) {
     // Initialize locations
     crossedBridge = new Location("Other side of the bridge", "As you cross the bridge, bridle in hand, you notice thick fog gathering around you. \nWhen you get to the other side and turn to wave goodbye to the troll, you notice that the bridge has been entirely enshrouded in the mist. \nAhead of you lies a gloomy-looking lake. ");
     lake = new Location("Lake", "A scaled horse-like creature is standing on the lake's rocky shore. You realize that this must be the kelpie, and prepare to capture it.", lake1);
-    lake1 = new Location("Lake", "Now subdued, the kelpie bends its head, as if inviting you onto its back. you on its back as a way reaching your next destination.");
-    crossedLake = new Location("Other side of the lake", "The Kelpie is lending its back to you as a way to cross the gloomy lake. In the distance you notice land that fits the description of Aine's domain. \nAs if reading your mind, the kelpie starts swimming across the lake as soon as you sit on it.");
+    lake1 = new Location("Lake", "Now subdued, the kelpie bends its head, as if inviting you on its back as a way reaching your next destination.");
+    crossedLake = new Location("Edge of the lake", "The Kelpie is lending its back to you as a way to cross the gloomy lake. In the distance you notice land that fits the description of Aine's domain. \nAs if reading your mind, the kelpie starts swimming across the lake as soon as you sit on it.\nYou are now on the other side of the lake.");
     swamp = new Location("Swamp", "The swamp is a quiet expanse of twisted trees and still waters, alive with the hums of insects and the croaks of hidden creatures. Thick vines grow from the trees.");
     courtyard = new Location("Courtyard",
         "You find yourself surrounded by a circle of dead grass, replacing the mushrooms that brought you here. \nThe air is filled with an otherworldly energy, and the sun is hidden behind the clouds");
@@ -144,6 +144,7 @@ void GameWorld::init(Player& player) {
 
     // Connect the locations
     courtyard->addConnectedLocation(greatHall);
+    courtyard->addConnectedLocation(crossedLake);
 
     greatHall->addConnectedLocation(banquetHall);
 
